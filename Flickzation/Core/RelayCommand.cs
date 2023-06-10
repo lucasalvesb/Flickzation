9d9588a95a6cfaa667f6a523c60d9d799adac92c
Lucas Alves <lucasalves94@gmail.com>
2023-06-10 17:42:58 -0300

    finished relay command

--- Flickzation/Core/RelayCommand.cs
+++ Flickzation/Core/RelayCommand.cs
@@ -18,11 +18,21 @@ namespace Flickzation.Core
             add { CommandManager.RequerySuggested += value;  }
             remove { CommandManager.RequerySuggested -= value;  }
         }
-    }
 
-    public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
-    {
-        _execute = execute; 
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
 
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
     }
 }
