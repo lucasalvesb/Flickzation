d94f1718f1074e47157d1db333be2269715a08e8
Lucas Alves <lucasalves94@gmail.com>
2023-06-10 23:23:06 -0300

    inserted font poppins

--- Flickzation/MainWindow.xaml.cs
+++ Flickzation/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Flickzation
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
