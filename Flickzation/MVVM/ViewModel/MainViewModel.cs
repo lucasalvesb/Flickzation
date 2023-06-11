d75207ad5568279251df90db57f6cd9d1ee65db1
Lucas Alves <lucasalves94@gmail.com>
2023-06-10 23:06:56 -0300

    front page of the app ui ready

--- Flickzation/MVVM/ViewModel/MainViewModel.cs
+++ Flickzation/MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,22 @@ namespace Flickzation.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public HomeViewModel HomeVM { get; set; }
+        
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel() 
         { 
-            
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
         }
     }
 }
